a50eed49236b17afc8bd9a8564ccb3ded50c3245
Your Name <your.email@example.com>
2025-09-03 10:09:26 +0530

    Added some extra code and fixed some issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,23 @@
 import { AuthProvider } from './hooks/useAuth'
-import { EmployeeProvider } from './hooks/useEmployee' // IMPORT THE NEW PROVIDER
+import { EmployeeProvider } from './hooks/useEmployee'
 import AppRouter from './components/layout/AppRouter'
 import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 
 function App() {
   return (
     <AuthProvider>
-      <EmployeeProvider> {/* WRAP THE ROUTER */}
+      <EmployeeProvider>
         <AppRouter />
       </EmployeeProvider>
     </AuthProvider>
   )
 }
 
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
